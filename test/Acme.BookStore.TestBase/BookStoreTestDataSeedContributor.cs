adc16f0683f6bc89694609ecaaf080e49d209d19
Aileen Rae <aileen@stormideas.com>
2019-09-09 17:50:41 +0100

    ABP tutorial part 3

--- test/Acme.BookStore.TestBase/BookStoreTestDataSeedContributor.cs
+++ test/Acme.BookStore.TestBase/BookStoreTestDataSeedContributor.cs
@@ -1,16 +1,49 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Volo.Abp.Data;
 using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Repositories;
+using Volo.Abp.Guids;
 
 namespace Acme.BookStore
 {
-    public class BookStoreTestDataSeedContributor : IDataSeedContributor, ITransientDependency
+    public class BookStoreTestDataSeedContributor
+        : IDataSeedContributor, ITransientDependency
     {
-        public Task SeedAsync(DataSeedContext context)
-        {
-            /* Seed additional test data... */
+        private readonly IRepository<Book, Guid> _bookRepository;
+        private readonly IGuidGenerator _guidGenerator;
 
-            return Task.CompletedTask;
+        public BookStoreTestDataSeedContributor(
+            IRepository<Book, Guid> bookRepository,
+            IGuidGenerator guidGenerator)
+        {
+            _bookRepository = bookRepository;
+            _guidGenerator = guidGenerator;
+        }
+
+        public async Task SeedAsync(DataSeedContext context)
+        {
+            await _bookRepository.InsertAsync(
+                new Book
+                {
+                    Id = _guidGenerator.Create(),
+                    Name = "1984",
+                    Type = BookType.ScienceFiction,
+                    PublishDate = new DateTime(1949, 06, 08),
+                    Price = 21
+                }
+            );
+
+            await _bookRepository.InsertAsync(
+                new Book
+                {
+                    Id = _guidGenerator.Create(),
+                    Name = "Spinning Silver",
+                    Type = BookType.Fantasy,
+                    PublishDate = new DateTime(2018, 07, 10),
+                    Price = 15
+                }
+            );
         }
     }
 }
